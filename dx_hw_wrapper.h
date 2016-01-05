70aa0c9321750a4c4aca11519717ee0a1004bde8
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-01-05 13:26:24 +0100

    add a copy of dx_hw video out

---
+++ dx_hw_wrapper.h
@@ -0,0 +1,11 @@
+/*
+ *			GPAC - Multimedia Framework C SDK
+ *
+ *			Authors: Romain Bouqueau
+ *			Copyright (c) Romain Bouqueau 2016
+ *					All rights reserved
+ *
+ *  This file is part of GPAC - sample GPAC player with shared OpenGL
+ *  shared context
+ *
+ */
